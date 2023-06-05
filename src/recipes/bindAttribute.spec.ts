f6873093263373a4c2432c719feebc2769403de3
malko <jgotti@jgotti.org>
2023-06-05 18:56:58 +0200

    ✨ (recipes) Add bindAttribute recipe

---
+++ src/recipes/bindAttribute.spec.ts
@@ -0,0 +1,107 @@
+/**
+ * @vitest-environment happy-dom
+ * // don't use jsdom as mutation observer are not triggered properly (delayed and with latest values)
+ */
+import { describe, it, expect } from "vitest"
+import { createStore } from "../store"
+import { bindAttribute } from "./bindAttribute"
+describe("bindAttribute", () => {
+	it("should bind a Store<String> to an attribute in both direction", () => {
+		const store = createStore("John")
+		const div = document.createElement("div")
+		const unbind = bindAttribute(store, div, "data-name")
+		expect(div.getAttribute('data-name')).toBe("John")
+		store.set("Jane")
+		expect(div.getAttribute('data-name')).toBe("Jane")
+		div.setAttribute('data-name', "Jack")
+		expect(store.get()).toBe("Jack")
+		unbind()
+		store.set("Jazz")
+		expect(div.dataset.name).toBe("Jack")
+		expect(store.get()).toBe("Jazz")
+	})
+
+	it("When attribute is not set should initialize attribute to store value if store value is set", () => {
+		const store = createStore<string|undefined>(null)
+		const div = document.createElement("div")
+		const settup = (storeValue) => {
+			div.removeAttribute("data-name")
+			store.set(storeValue)
+			const unbind = bindAttribute(store, div, "data-name")
+			unbind()
+		}
+		settup("John")
+		expect(div.getAttribute("data-name")).toBe("John")
+		expect(store.get()).toBe("John")
+
+		settup("") // empty string is a value
+		expect(store.get()).toBe("")
+		expect(div.getAttribute("data-name")).toBe("")
+
+		// store has no value at all so attribute should not be set
+		settup(undefined)
+		expect(div.getAttribute("data-name")).toBe(null)
+		expect(store.get()).toBe(undefined)
+
+		settup(null)
+		expect(store.get()).toBe(null)
+		expect(div.getAttribute("data-name")).toBe(null)
+	})
+
+	it("When attribute is set to '' should initialize attribute to store value if store value is not empty but store will be set to empty string if null or undefined", () => {
+		const store = createStore<string|undefined>(null)
+		const div = document.createElement("div")
+		const settup = (storeValue) => {
+			div.setAttribute("data-name", "")
+			store.set(storeValue)
+			const unbind = bindAttribute(store, div, "data-name")
+			unbind()
+		}
+		settup("John")
+		expect(div.getAttribute("data-name")).toBe("John")
+		expect(store.get()).toBe("John")
+
+		settup("")
+		expect(store.get()).toBe("")
+		expect(div.getAttribute("data-name")).toBe("")
+
+		// store is not set but we have an empty value set store to empty string
+		settup(undefined)
+		expect(div.getAttribute("data-name")).toBe("")
+		expect(store.get()).toBe("")
+
+		settup(null)
+		expect(div.getAttribute("data-name")).toBe("")
+		expect(store.get()).toBe("")
+
+	})
+
+	it("should initialize the store to attribute value if store value is empty and attibute is not", () => {
+		const store = createStore<string|undefined>(null)
+		const div = document.createElement("div")
+		const settup = (storeValue:any) => {
+			store.set(storeValue)
+			div.setAttribute("data-name", "John")
+			const unbind = bindAttribute(store, div, "data-name")
+			unbind()
+		}
+		// store has a value it prevail on attribute value
+		settup("Jane")
+		expect(div.getAttribute("data-name")).toBe("Jane")
+		expect(store.get()).toBe("Jane")
+
+		// not empty value on attribute win over empty store value
+		settup("")
+		expect(div.getAttribute("data-name")).toBe("John")
+		expect(store.get()).toBe("John")
+
+		// store not set to a value should take attribute value
+		settup(undefined)
+		expect(div.getAttribute("data-name")).toBe("John")
+		expect(store.get()).toBe("John")
+
+		settup(null)
+		expect(store.get()).toBe("John")
+		expect(div.getAttribute("data-name")).toBe("John")
+	})
+})
---
+++ src/recipes/bindAttribute.ts
@@ -0,0 +1,49 @@
+import { StoreInterface } from "../type"
+
+
+/**
+ * Only applicable to string value store
+ * it will make a 2 way data binding between the store value and the attribute of the element and return an unbind function
+ * Usage:
+ * ```ts
+ * const store = createStore({username: "John"})
+ * const element = document.querySelector("div#hello")
+ * const unbind = bindAttribute(store.getScopeStore("username"), element, "data-hello")
+ * // now the attribute data-hello of the div#hello will always be in sync with the store value
+ *
+ * unbind()
+ * // now the attribute data-hello of the div#hello will not be in sync with the store value anymore
+ * ```
+ */
+export const bindAttribute = <TypeState extends string|undefined|null>(store: StoreInterface<TypeState>, element:HTMLElement, attributeName:string) => {
+	// initial sync values for store & attribute
+	const storeInitVal = store.get()
+	const attrInitVal = element.getAttribute(attributeName)
+	if (storeInitVal || (storeInitVal==="" && attrInitVal === null)) {
+		element.setAttribute(attributeName, storeInitVal)
+	} else if (attrInitVal !== null && !storeInitVal) {
+		store.set(attrInitVal as TypeState)
+	}
+	// observe attribute changes
+	const mutationObserver = new MutationObserver(mutations => {
+		mutations.forEach(mutation => {
+			if (mutation.type === "attributes" && mutation.attributeName === attributeName) {
+				const newState = (mutation.target as HTMLElement).getAttribute(attributeName) as TypeState
+				store.get() !== newState && store.set(newState)
+			}
+		})
+	})
+	mutationObserver.observe(element, { attributes: true, attributeFilter: [attributeName] })
+	// observe store changes
+	const unsubscribe = store.subscribe((newState) => {
+		if (newState === undefined || newState === null) {
+			element.removeAttribute(attributeName)
+		} else {
+			element.setAttribute(attributeName, String(newState))
+		}
+	})
+	return () => {
+		mutationObserver.disconnect()
+		unsubscribe()
+	}
+}
