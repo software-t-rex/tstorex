03940a5f6a1f615ca91e8db351ac4a2259398ba4
malko <jgotti@jgotti.org>
2023-06-06 16:39:01 +0200

    ✨ (recipes:mutate): Introduce new mutate recipe

---
+++ src/recipes/mutate.spec.ts
@@ -0,0 +1,148 @@
+import { describe, it, expect, vi } from "vitest"
+import { createStore } from "../store"
+import { produce, removeProp, setProp, mutate } from "./mutate"
+
+const john = {
+	name: "John",
+	age: 42
+}
+const NotDraftableErrorMatcher = "state must be a plain object or an array"
+
+describe("produce", () => {
+	it("should allow recipes to mutate the received state", () => {
+		const mutated = produce({ name: "John", age: 42 }, (draft) => {
+			draft.name = "Jane"
+			draft.age = 44
+		})
+		expect(mutated).toEqual({ name: "Jane", age: 44 })
+		const mutated2 = produce(mutated, (draft) => {
+			delete draft.age
+			draft.pet = "cat"
+		})
+		expect(mutated2).toEqual({ name: "Jane", pet: "cat" })
+		expect(mutated).toEqual({ name: "Jane", age: 44 })
+	})
+	it("should not mutate the original object", () => {
+		const o = { name: "John" }
+		Object.freeze(o)
+		const mutated = produce(o, (draft) => {
+			draft.name = "Jane"
+		})
+		expect(mutated).toEqual({ name: "Jane" })
+		expect(o).toEqual({ name: "John" })
+	})
+	it("should allow to return a brand new object", () => {
+		const mutated = produce({ name: "John" }, () => {
+			return { name: "Jane", age: 44 }
+		})
+		expect(mutated).toEqual({ name: "Jane", age: 44 })
+	})
+	it("should allow to mutate nested properties", () => {
+		const mutated = produce<{ address: { street: string, city?: string } }>({ address: { street: "Main St", city: "Paris" } }, (draft) => {
+			draft.address.street = "Wall St"
+			delete draft.address.city;
+		})
+		expect(mutated).toEqual({ address: { street: "Wall St" } })
+	})
+	it("should return SAME object (same ref) If no mutation applyed", () => {
+		const o = { name: "John" }
+		const mutated = produce(o, (s) => { })
+		expect(mutated === o).toBe(true)
+	})
+	it("should throw if draft is returned", () => {
+		const o = { name: "John" }
+		expect(() => produce(o, (s) => {
+			s.name = "Jane"
+			return s
+		})).toThrow("recipe must not return draft")
+	})
+	it("should work with array", () => {
+		const mutated = produce([1, 2, 3], (draft) => {
+			draft.push(4)
+		})
+		expect(mutated).toEqual([1, 2, 3, 4])
+	})
+	it("should throw if state is not draftable", () => {
+		expect(() => produce(42, (s) => { })).toThrow(NotDraftableErrorMatcher)
+		expect(() => produce("a string", (s) => { })).toThrow(NotDraftableErrorMatcher)
+		expect(() => produce(new Date(), (s) => { })).toThrow(NotDraftableErrorMatcher)
+	})
+})
+
+describe("removeProp", () => {
+	it("should remove a property from state in given store", () => {
+		const store = createStore(john)
+		removeProp(store, "age")
+		expect(store.get()).toEqual({ name: "John" })
+		expect(store.get().age).toBeUndefined()
+	})
+	it("should allow to remove nested properties from given store", () => {
+		const store = createStore({ address: { street: "Main St", city: "Paris" } })
+		removeProp(store, "address.city")
+		expect(store.get()).toEqual({ address: { street: "Main St" } })
+		expect(store.get().address.city).toBeUndefined()
+	})
+})
+
+describe("setProp", () => {
+	it("should set a state property in given store", () => {
+		const store = createStore(john)
+		setProp(store, "age", 44)
+		expect(store.get()).toEqual({ name: "John", age: 44 })
+	})
+	it("should allow to set nested properties into given store", () => {
+		const store = createStore<{address:{street:string, city?:string}}>({ address: { street: "Main St" } })
+		setProp(store, "address.city", "Paris")
+		setProp(store, "address.street", "Wall St")
+		expect(store.get()).toEqual({ address: { street: "Wall St", city: "Paris" } })
+	})
+})
+
+describe("mutate", () => {
+	it("should mutate the state in given store", () => {
+		const store = createStore(john)
+		mutate(store, (s) => {
+			s.name = "Jane"
+		})
+		expect(store.get()).toEqual({ name: "Jane", age: 42 })
+	})
+	it("should allow to mutate nested properties into given store", () => {
+		const store = createStore<{address:{street?:string, city?:string}}>({ address: { street: "Main St" } })
+		mutate(store, (s) => {
+			delete s.address.street
+			s.address.city = "Paris"
+		})
+		expect(store.get()).toEqual({ address: { city: "Paris" } })
+	})
+	it("should allow to set a brand new object", () => {
+		const store = createStore(john)
+		mutate(store, () => {
+			return { name: "Jane", age: 44 }
+		})
+		expect(store.get()).toEqual({ name: "Jane", age: 44 })
+	})
+	it("should not trigger any change if no mutation applyed", () => {
+		const fn = vi.fn()
+		const store = createStore(john)
+		const unsubscibe = store.subscribe(() => {})
+		expect(fn).not.toHaveBeenCalled()
+		unsubscibe()
+	})
+	it("should throw if draft is returned", () => {
+		const store = createStore(john)
+		expect(() => mutate(store, (s) => {
+			return s
+		})).toThrow("recipe must not return draft")
+	})
+	it("should work with array", () => {
+		const store = createStore([1, 2, 3])
+		mutate(store, (s) => {
+			s.push(4)
+		})
+		expect(store.get()).toEqual([1, 2, 3, 4])
+	})
+	it("should throw if state is not draftable", () => {
+		expect(() => mutate(createStore("a string"), (s) => { })).toThrow(NotDraftableErrorMatcher)
+		expect(() => mutate(createStore(42), (s) => { })).toThrow(NotDraftableErrorMatcher)
+	})
+})
---
+++ src/recipes/mutate.ts
@@ -0,0 +1,182 @@
+import { ScopePath, ScopeValue, Store } from "../type"
+
+const innerDraftSymbol = Symbol('draft')
+
+/** API inspiration from immer (implem is not) */
+interface Draft<T> {
+	state: T,
+	draft: T,
+	isModified: () => boolean,
+	setModified: () => void,
+	[innerDraftSymbol]: any,
+}
+type PropIdentifier = string | number | symbol
+
+/** A recipe can modify a draftState received directly or return a totally new state. */
+type Recipe<T> = (draftState: T) => void | any
+
+/** basic shallowCopy */
+const shallowCopy = (val: any) => {
+	if (Array.isArray(val)) { return [...val] }
+	if (val && typeof val === 'object') { return { ...val } }
+	return val
+}
+/** method to return a proper object from a draftProxy */
+const unProxyDraft = (draftState: any) => {
+	if (draftState && typeof draftState === 'object') {
+		Object.entries(draftState).forEach(([prop, val]) => {
+			if (val && typeof val === 'object' && (innerDraftSymbol in val)) {
+				draftState[prop] = draftState[prop][innerDraftSymbol]
+			}
+		})
+	}
+	return draftState
+}
+/* Proxy handler methods for draftProxy */
+const draftHandler: ProxyHandler<any> = {
+	ownKeys<T>(target: Draft<T>) { return Reflect.ownKeys(target.draft as Object) },
+	has<T>(target: Draft<T>, prop: PropIdentifier) {
+		if (prop === innerDraftSymbol) {
+			return true
+		}
+		return prop in (target.draft as Object)
+	},
+	get<T>(target: Draft<T>, prop: PropIdentifier) {
+		const { draft } = target as Draft<any>
+		if (prop === innerDraftSymbol || prop === 'toJSON') {
+			const res = target.isModified() ? unProxyDraft(draft) : target.state
+			return prop === innerDraftSymbol
+				? res
+				: 'toJSON' in target ? target.toJSON : () => res
+		}
+		if (typeof draft[prop] === 'object' && !(innerDraftSymbol in draft[prop])) {
+			const draftedProp = createDraft(draft[prop], target)
+			draft[prop] = draftedProp
+		}
+		return draft[prop]
+	},
+	set<T>(target: Draft<T>, prop: PropIdentifier, value: any) {
+		target.setModified()
+		;(target.draft as any)[prop] = value
+		return true
+	},
+	deleteProperty<T>(target: Draft<T>, prop: PropIdentifier) {
+		target.setModified()
+		return delete (target.draft as any)?.[prop]
+	}
+}
+/** return a new draftProxy */
+const createDraft = <T>(state: T, context?: Draft<T>): Draft<T> => {
+	let modified = false
+	const contextSetModified = context?.setModified
+	const setModified = () => {
+		if (!modified) {
+			contextSetModified?.()
+			modified = true
+		}
+	}
+	const draft = {
+		[innerDraftSymbol]:true,
+		state,
+		draft: shallowCopy(state),
+		isModified: () => modified,
+		setModified,
+	}
+	return new Proxy(draft, draftHandler as ProxyHandler<Draft<T>>)
+}
+const isDraftable = (o: any) => {
+	const type = Object.prototype.toString.call(o).match(/\[object ([a-zA-Z]+)\]/)?.[1]
+	return type === 'Object' || type === 'Array'
+}
+const throwIfNotDraftable = (o: any, prefix:string = 'createDraft') => {
+	if (!isDraftable(o)) {
+		throw new Error(`${prefix}: state must be a plain object or an array`)
+	}
+}
+/**
+ * take a recipe (draftState) => void | newState
+ * the recipe can modify the draftState received directly
+ * or return a totally new state.
+ * Beware it can return the original state object if no mutation happened on the draft
+ */
+export const produce = <T>(state: T, recipe: Recipe<T>) => {
+	throwIfNotDraftable(state, 'produce Error')
+	const draft = createDraft(state)
+	const res = recipe(draft as any)
+	if (typeof res === 'object' && innerDraftSymbol in res) {
+		throw new Error('produce ERROR: recipe must not return draft object')
+	}
+	return res === undefined ? draft[innerDraftSymbol] : res
+}
+
+/**
+ * take a recipe (draftState) => void | newState
+ * @example
+ * ```js
+ * import {mutateExtender} from 'store/extensions'
+ * const store = createStore({firstName: 'John', lastName: 'Doe'})
+ * const onlyFullNameRecipe = (draft) => {
+ * 	 draft.fullName = `${draft.firstName} ${draft.lastName}`
+ * 	 delete draft.firstName
+ *   delete draft.lastName
+ *   // for this example you could have done this instead:
+ *   // return {fullName: `${draft.firstName} ${draft.lastName}`}
+ * }
+ * mutate(store, onlyFullNameRecipe)
+ * console.log(store.get().fullName) // John Doe
+ * ```
+ * if anything other than undefined is returned by the recipe it will be considered as the new state to assign
+ * the recipe received a draftState that can be freely manipulated
+ * Warning: this method is intended to work with simple dataset objects like JSON objects
+ *   manipilating other values than plain objects or arrays is not in the scope.
+ *   It's perfect to add/remove/update properties on a plain object.
+ *   So if you need to define properties (Object.defineProperty)
+ *   or manipulate promises, maps, sets or more complex objects,
+ *   you should use the standard store.set method instead.
+ *   Anyway if you encounter a case that don't work don't hesitate to report
+ *   and we will decide if we want to handle it or not.
+ */
+export const mutate = <TypeState>(store: Store<TypeState>, recipe: (draftState: TypeState) => void | TypeState) => {
+	const state = store.get()
+	throwIfNotDraftable(state, 'mutate Error')
+	const newState = produce(state, recipe)
+	newState !== state && store.set(newState)
+}
+
+/**
+ * remove a given property from the store
+ * You can point nested properties using dot notation ie: removeProp(store, "pet.name")
+ */
+export const removeProp = <TypeState>(store: Store<TypeState>, prop: ScopePath<TypeState>) => {
+	const props = prop.split('.')
+	const lastKey = props.pop() as string
+	let {get, set} = store as any
+	let newState;
+	if (!props.length) {
+		newState = shallowCopy(get())
+	} else {
+		({get, set} = store.getScopeStore(props.join('.') as any))
+		newState = shallowCopy(get())
+	}
+	delete newState?.[lastKey]
+	set(newState)
+}
+
+/**
+ * set a given property in the store to given value
+ * You can point nested properties using dot notation ie: ```setProp(store, "pet.name", "Rex")```
+ */
+export const setProp = <TypeState, Key extends ScopePath<TypeState>>(store: Store<TypeState>, prop: Key, value: ScopeValue<TypeState, Key>) => {
+	const props = prop.split('.')
+	const lastKey = props.pop() as string
+	let newState;
+	let {get, set} = store as any
+	if (!props.length) {
+		newState = shallowCopy(get())
+	} else {
+		({get, set} = store.getScopeStore(props.join('.') as any))
+		newState = shallowCopy(get())
+	}
+	newState[lastKey] = value
+	set(newState)
+}
